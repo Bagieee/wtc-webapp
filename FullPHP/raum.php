f9c0dfca00dff83555ae30cf74b0877d1b06a327
Bagieee <50483054+Bagieee@users.noreply.github.com>
2022-01-13 08:23:22 +0100

    UIKIT und BOOTSTRAP in raum.php hinzugefügt

--- FullPHP/raum.php
+++ FullPHP/raum.php
@@ -4,6 +4,12 @@
     <head>
     <title>WTC - WorkstationToolCheck</title>
     <link href="Styles/raum.css" rel="stylesheet">
+
+    <!-- UIKIT und BOOTSTRAP EINBINDUNG -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/js/uikit-icons.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </head>
     <body>
     <div id="titel">
