d6efb20110b4aa0746d6c93daa1a9f9218888ebe
Bagieee <50483054+Bagieee@users.noreply.github.com>
2022-01-17 12:16:22 +0100

    UIKIT & Bootstrap gelöscht und Seiten angepasst

--- FullPHP/index.php
+++ FullPHP/index.php
@@ -5,12 +5,9 @@
         <title>WTC - WorkstationToolCheck</title>
         <link href="Styles/index.css" rel="stylesheet">
         <link rel="icon" href="favicon.png">
-        
-        <!-- UIKIT und BOOTSTRAP EINBINDUNG -->
-        <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/js/uikit.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/js/uikit-icons.min.js"></script>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+        <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/130527/qrcode.js"></script>  
     </head>
     <body>
         <div id="container">
--- FullPHP/qrcode-gen.php
+++ FullPHP/qrcode-gen.php
@@ -8,12 +8,6 @@
 
         <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
         <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/130527/qrcode.js"></script>  
-
-        <!-- UIKIT und BOOTSTRAP EINBINDUNG -->
-        <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/js/uikit.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/uikit@3.9.4/dist/js/uikit-icons.min.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </head>
     <body>
         <div id="container">
@@ -86,18 +80,8 @@
         <button id="gen_btn" class="btn_a" <?=(isset($_GET['tischid']) && isset($_GET['raumid']))?'':'disabled';?> onclick="generateCode()">QR-CODE GENERIEREN</button>
 
         <div id="qrcode"></div>
-        
-     
 
-        <!-- MUSS NOCH ANGEPASST WERDEN -> FARBE GRÜN UND BLINKEND WENN FORM AUSGEFÜLLT -->
-        <!-- <button id="checkSub_btn"class="btn_a">QR-CODE BESTÄTIGEN UND ABSCHICKEN</button> -->
-            
-            
-            
-            
-            
-            
-            
+
         </div>
     </body>
 
