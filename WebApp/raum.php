494f661d68aba9cf4efa44a1ae75dba18c59b2ca
Bagieee <50483054+Bagieee@users.noreply.github.com>
2022-02-20 15:08:06 +0100

    Implemented Auto refresh

--- WebApp/raum.php
+++ WebApp/raum.php
@@ -5,6 +5,7 @@
         <title>WTC - WorkstationToolCheck</title>
         <link href="Styles/raum.css" rel="stylesheet">
         <link rel="icon" href="favicon.png">
+        <meta http-equiv="refresh" content="5" >
 
         <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
         <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/130527/qrcode.js"></script>  
