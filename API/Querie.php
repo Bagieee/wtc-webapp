79d22adb0af10e0e617cdbaf951f573a09c2b61a
Schmuurf <tm.isaac.sebastiani@protonmail.com>
2022-03-15 12:08:46 +0100

    API for URL

--- API/Querie.php
+++ API/Querie.php
@@ -1,5 +1,8 @@
 <?php
 
+    class urlValues{
+        public $picUrl;
+    }
     class Querie {
         private $pdo;
         
@@ -14,7 +17,7 @@
             $this->pdo = $db;
         }
 
-        //Scan übermitteln
+        //Positiven Scan übermitteln
         public function scanPos() {
               
             $stmt = $this->pdo->prepare('INSERT INTO tblscan (scanTischId, scanErgebniss, scanName) values ((select tischId from tblTisch where tischRaumId = ? AND tischNummer = ? ), ? , ? )');
@@ -29,6 +32,7 @@
             }
         }
 
+        //Negativen Scan übermitteln
         public function scanNeg() {
             $stmt = $this->pdo->prepare('INSERT INTO tblscan (scanTischId, scanErgebniss, scanName, scanKommentar) values ((select tischId from tblTisch where tischRaumId = ? AND tischNummer = ? ), ? , ? , ? )');
 
@@ -43,6 +47,7 @@
             }
         }
 
+        //Kommentar übermitteln
         public function comment() {
             $stmt = $this->pdo->prepare('INSERT INTO tblKommentar (kommentarTischId, kommentarName, kommentarText) values ((select tischId from tblTisch where tischRaumId = ? AND tischNummer = ? ), ? , ? )');
 
@@ -54,6 +59,20 @@
             if($stmt->execute([$this->tischRaumId, $this->tischNummer, $this->name, $this->kommentar])) {
                 return true;
             }
+        }
+
+     
+
+        public function url() {
+            $this->tischNummer = htmlspecialchars(strip_tags($this->tischNummer));
+            $stmt = $this->pdo->prepare('SELECT picUrl from tblpic where picRaumId = ? LIMIT 1');
+            $stmt->execute([$this->tischNummer]);
+            $url = "";
+            foreach ($stmt->fetchAll() as $row)
+            {
+                $url = $row["picUrl"];
+            }
+            return $url;
         }
     }
 ?>
